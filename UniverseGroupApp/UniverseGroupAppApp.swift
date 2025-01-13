6a27129f7bce733f61d853324e4e62c91d9ec59f
roman.leibiuk <roman.leibiuk@vakoms.com>
2025-01-13 10:37:42 +0100

    Initial commit

---
+++ UniverseGroupApp/UniverseGroupAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniverseGroupAppApp.swift
+//  UniverseGroupApp
+//
+//  Created by Roman Leibiuk on 13.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct UniverseGroupAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
